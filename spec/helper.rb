e2374554d975298b98fb39ff19e13f4c901719a6
Paul Scarrone <paul.scarrone@gmail.com>
2017-06-09 11:40:05 -0400

    ditch code coverage for now

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,17 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("..", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-if RUBY_VERSION >= "1.9"
-  require "simplecov"
-  require "coveralls"
-
-  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
-
-  SimpleCov.start do
-    minimum_coverage(78.48)
-  end
-end
-
 require "rspec"
 require "rack/test"
 require "webmock/rspec"
